21a226cf691d4982c7b639a985d38563986929e6
Alejandro <averagonzalez@omnitech-inc.com>
2021-01-04 11:37:30 -0600

    Add Prediction Data Model

---
+++ RideSharePrediction/RideSharePrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace RideSharePrediction
+{
+    public class RideSharePrediction
+    {
+        [ColumnName("Score")]
+        public float RideAmount; 
+    }
+}
